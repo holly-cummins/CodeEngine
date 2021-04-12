bba824594ea9a04f54ab94543e4de2331b463a2f
Holly Cummins <cumminsh@uk.ibm.com>
2021-04-12 21:20:46 +0100

    Add duck graphic

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const http = require("http");
 
+const duck = require("./duck");
+
 http
   .createServer(function (request, response) {
     target = process.env.TARGET ? process.env.TARGET : "World";
     msg = process.env.MSG ? process.env.MSG : "Hello " + target + "\n";
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end(msg);
+    response.end(duck + msg);
   })
   .listen(8080);
 
